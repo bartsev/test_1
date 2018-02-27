4c6c67cb7cbb8356a81eb640120358c7460b5858
Alexander Bartsev <bartsev@inbox.ru>
2018-02-27 22:24:37 +0400

    Initial commit

---
+++ fruits/index.php
@@ -0,0 +1,6 @@
+<?require($_SERVER["DOCUMENT_ROOT"]."/bitrix/header.php");?>
+
+<?$APPLICATION->IncludeComponent('ylab:fruits', '', Array('IBLOCK_ID'=>15));?>
+
+<?require($_SERVER["DOCUMENT_ROOT"]."/bitrix/footer.php");?>
+
---
+++ local/components/ylab/fruits/class.php
@@ -0,0 +1,27 @@
+<?php
+
+use \Bitrix\Iblock\ElementTable;
+
+class FruitsComponent extends CBitrixComponent
+{
+	private function getFruits()
+	{
+		$dbResult = ElementTable::getList(
+			Array(
+				'select'=>Array('ID', 'NAME'),
+				'filter'=>Array('IBLOCK_ID'=>$this->arParams['IBLOCK_ID'])
+			));
+
+		return $dbResult->fetchAll();
+
+    }
+
+    public function executeComponent()
+    {
+        global $APPLICATION;
+        $APPLICATION->RestartBuffer();
+        $this->arResult = $this->getFruits();
+
+        $this->includeComponentTemplate();
+    }
+}
